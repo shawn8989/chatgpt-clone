3e5cf9778d1ff8f523b9a04edfb5d5e253cce9d1
shawn8989 <78062697+shawn8989@users.noreply.github.com>
2023-01-04 05:18:32 -0800

    changes the wording and prompt settings

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import gradio as gr
 #openai.api_key = os.getenv("OPENAI_API_KEY")
 
 #if you have OpenAI API key as a string, enable the below
-openai.api_key = "xxxxxx"
+openai.api_key = "api_key"
 
 start_sequence = "\nAI:"
 restart_sequence = "\nHuman: "
@@ -44,7 +44,7 @@ block = gr.Blocks()
 
 
 with block:
-    gr.Markdown("""<h1><center>Build Yo'own ChatGPT with OpenAI API & Gradio</center></h1>
+    gr.Markdown("""<h1><center>Design. Build Your Dreams</center></h1>
     """)
     chatbot = gr.Chatbot()
     message = gr.Textbox(placeholder=prompt)
